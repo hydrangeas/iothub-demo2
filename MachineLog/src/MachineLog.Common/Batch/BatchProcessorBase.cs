da45d0ca06bff427d27efc5dce51053a5f4f3bee
Akira Otaka <hydrangeas.project@gmail.com>
2025-03-27 15:11:26 +0900

    バッチ処理機能を実装

---
+++ MachineLog/src/MachineLog.Common/Batch/BatchProcessorBase.cs
@@ -0,0 +1,317 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MachineLog.Common.Batch;
+
+/// <summary>
+/// バッチ処理の基本クラス
+/// </summary>
+/// <typeparam name="T">バッチ処理の対象となる型</typeparam>
+public abstract class BatchProcessorBase<T> : IBatchProcessor<T>, IDisposable where T : class
+{
+  private readonly BatchQueue<T> _queue;
+  private readonly BatchProcessorOptions _options;
+  private readonly List<T> _currentBatch;
+  private readonly SemaphoreSlim _batchLock;
+  private readonly Timer _batchTimer;
+  private readonly Timer _idleTimer;
+  private readonly CancellationTokenSource _cts;
+  private bool _isProcessing;
+  private bool _isDisposed;
+  private DateTime _lastItemAddedTime;
+
+  /// <summary>
+  /// バッチ処理の基本クラスを初期化する
+  /// </summary>
+  /// <param name="options">バッチ処理のオプション</param>
+  protected BatchProcessorBase(BatchProcessorOptions options)
+  {
+    _options = options ?? BatchProcessorOptions.Default;
+    _queue = new BatchQueue<T>(_options.BatchQueueCapacity);
+    _currentBatch = new List<T>();
+    _batchLock = new SemaphoreSlim(1, 1);
+    _cts = new CancellationTokenSource();
+    _lastItemAddedTime = DateTime.UtcNow;
+
+    // バッチ処理タイマーの初期化
+    _batchTimer = new Timer(
+        ProcessBatchTimerCallback,
+        null,
+        Timeout.Infinite,
+        Timeout.Infinite);
+
+    // アイドルタイマーの初期化
+    _idleTimer = new Timer(
+        IdleTimeoutCallback,
+        null,
+        Timeout.Infinite,
+        Timeout.Infinite);
+  }
+
+  /// <summary>
+  /// アイテムをバッチに追加する
+  /// </summary>
+  /// <param name="item">追加するアイテム</param>
+  /// <returns>追加が成功したかどうかを示す非同期タスク</returns>
+  public async Task<bool> AddAsync(T item)
+  {
+    if (_isDisposed)
+      throw new ObjectDisposedException(nameof(BatchProcessorBase<T>));
+
+    if (item == null)
+      throw new ArgumentNullException(nameof(item));
+
+    _lastItemAddedTime = DateTime.UtcNow;
+    return await _queue.EnqueueAsync(item);
+  }
+
+  /// <summary>
+  /// 複数のアイテムをバッチに追加する
+  /// </summary>
+  /// <param name="items">追加するアイテムのコレクション</param>
+  /// <returns>追加が成功したかどうかを示す非同期タスク</returns>
+  public async Task<bool> AddRangeAsync(IEnumerable<T> items)
+  {
+    if (_isDisposed)
+      throw new ObjectDisposedException(nameof(BatchProcessorBase<T>));
+
+    if (items == null)
+      throw new ArgumentNullException(nameof(items));
+
+    _lastItemAddedTime = DateTime.UtcNow;
+    return await _queue.EnqueueRangeAsync(items);
+  }
+
+  /// <summary>
+  /// 現在のバッチを強制的に処理する
+  /// </summary>
+  /// <returns>処理が成功したかどうかを示す非同期タスク</returns>
+  public async Task<bool> FlushAsync()
+  {
+    if (_isDisposed)
+      throw new ObjectDisposedException(nameof(BatchProcessorBase<T>));
+
+    return await ProcessBatchAsync();
+  }
+
+  /// <summary>
+  /// バッチ処理を開始する
+  /// </summary>
+  /// <returns>開始が成功したかどうかを示す非同期タスク</returns>
+  public Task<bool> StartAsync()
+  {
+    if (_isDisposed)
+      throw new ObjectDisposedException(nameof(BatchProcessorBase<T>));
+
+    // バッチ処理タイマーを開始
+    _batchTimer.Change(
+        _options.BatchIntervalInMilliseconds,
+        _options.BatchIntervalInMilliseconds);
+
+    // アイドルタイマーを開始
+    _idleTimer.Change(
+        _options.IdleTimeoutInMilliseconds,
+        _options.IdleTimeoutInMilliseconds);
+
+    // バッチ処理ループを開始
+    Task.Run(ProcessBatchLoopAsync);
+
+    return Task.FromResult(true);
+  }
+
+  /// <summary>
+  /// バッチ処理を停止する
+  /// </summary>
+  /// <returns>停止が成功したかどうかを示す非同期タスク</returns>
+  public async Task<bool> StopAsync()
+  {
+    if (_isDisposed)
+      throw new ObjectDisposedException(nameof(BatchProcessorBase<T>));
+
+    // タイマーを停止
+    _batchTimer.Change(Timeout.Infinite, Timeout.Infinite);
+    _idleTimer.Change(Timeout.Infinite, Timeout.Infinite);
+
+    // 残りのバッチを処理
+    await ProcessBatchAsync();
+
+    // キャンセルトークンをキャンセル
+    _cts.Cancel();
+
+    return true;
+  }
+
+  /// <summary>
+  /// 現在のバッチサイズを取得する
+  /// </summary>
+  /// <returns>現在のバッチサイズ（バイト単位）</returns>
+  public int GetCurrentBatchSize()
+  {
+    return CalculateBatchSize(_currentBatch);
+  }
+
+  /// <summary>
+  /// 現在のバッチのエントリ数を取得する
+  /// </summary>
+  /// <returns>現在のバッチのエントリ数</returns>
+  public int GetCurrentBatchCount()
+  {
+    return _currentBatch.Count;
+  }
+
+  /// <summary>
+  /// バッチ処理のオプションを取得する
+  /// </summary>
+  /// <returns>バッチ処理のオプション</returns>
+  public BatchProcessorOptions GetOptions()
+  {
+    return _options;
+  }
+
+  /// <summary>
+  /// バッチのサイズを計算する
+  /// </summary>
+  /// <param name="batch">サイズを計算するバッチ</param>
+  /// <returns>バッチのサイズ（バイト単位）</returns>
+  protected abstract int CalculateBatchSize(List<T> batch);
+
+  /// <summary>
+  /// バッチを処理する
+  /// </summary>
+  /// <param name="batch">処理するバッチ</param>
+  /// <returns>処理が成功したかどうかを示す非同期タスク</returns>
+  protected abstract Task<bool> ProcessBatchItemsAsync(List<T> batch);
+
+  /// <summary>
+  /// バッチ処理ループを実行する
+  /// </summary>
+  private async Task ProcessBatchLoopAsync()
+  {
+    try
+    {
+      while (!_cts.Token.IsCancellationRequested)
+      {
+        // キューからアイテムを取得
+        var items = await _queue.DequeueMultipleAsync(
+            _options.MaxBatchCount,
+            _options.IdleTimeoutInMilliseconds);
+
+        if (items.Count > 0)
+        {
+          await _batchLock.WaitAsync(_cts.Token);
+          try
+          {
+            _currentBatch.AddRange(items);
+
+            // バッチサイズまたはカウントが上限に達した場合、処理を実行
+            if (GetCurrentBatchSize() >= _options.MaxBatchSizeInBytes ||
+                GetCurrentBatchCount() >= _options.MaxBatchCount)
+            {
+              await ProcessCurrentBatchAsync();
+            }
+          }
+          finally
+          {
+            _batchLock.Release();
+          }
+        }
+      }
+    }
+    catch (OperationCanceledException)
+    {
+      // キャンセルされた場合は正常終了
+    }
+    catch (Exception ex)
+    {
+      // エラーログ出力など
+      Console.Error.WriteLine($"バッチ処理ループでエラーが発生しました: {ex}");
+    }
+  }
+
+  /// <summary>
+  /// バッチタイマーのコールバック
+  /// </summary>
+  private async void ProcessBatchTimerCallback(object state)
+  {
+    await ProcessBatchAsync();
+  }
+
+  /// <summary>
+  /// アイドルタイムアウトのコールバック
+  /// </summary>
+  private async void IdleTimeoutCallback(object state)
+  {
+    var idleTime = DateTime.UtcNow - _lastItemAddedTime;
+    if (idleTime.TotalMilliseconds >= _options.IdleTimeoutInMilliseconds && GetCurrentBatchCount() > 0)
+    {
+      await ProcessBatchAsync();
+    }
+  }
+
+  /// <summary>
+  /// バッチを処理する
+  /// </summary>
+  private async Task<bool> ProcessBatchAsync()
+  {
+    if (_isProcessing || GetCurrentBatchCount() == 0)
+      return true;
+
+    await _batchLock.WaitAsync();
+    try
+    {
+      _isProcessing = true;
+      return await ProcessCurrentBatchAsync();
+    }
+    finally
+    {
+      _isProcessing = false;
+      _batchLock.Release();
+    }
+  }
+
+  /// <summary>
+  /// 現在のバッチを処理する
+  /// </summary>
+  private async Task<bool> ProcessCurrentBatchAsync()
+  {
+    if (_currentBatch.Count == 0)
+      return true;
+
+    var batchToProcess = new List<T>(_currentBatch);
+    _currentBatch.Clear();
+
+    return await ProcessBatchItemsAsync(batchToProcess);
+  }
+
+  /// <summary>
+  /// リソースを解放する
+  /// </summary>
+  public void Dispose()
+  {
+    Dispose(true);
+    GC.SuppressFinalize(this);
+  }
+
+  /// <summary>
+  /// リソースを解放する
+  /// </summary>
+  /// <param name="disposing">マネージドリソースを解放するかどうか</param>
+  protected virtual void Dispose(bool disposing)
+  {
+    if (_isDisposed)
+      return;
+
+    if (disposing)
+    {
+      _batchTimer.Dispose();
+      _idleTimer.Dispose();
+      _batchLock.Dispose();
+      _cts.Dispose();
+      _queue.Dispose();
+    }
+
+    _isDisposed = true;
+  }
+}
---
+++ MachineLog/src/MachineLog.Common/Batch/BatchQueue.cs
@@ -0,0 +1,189 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace MachineLog.Common.Batch;
+
+/// <summary>
+/// バッチキューを表すクラス
+/// </summary>
+/// <typeparam name="T">キューに格納する型</typeparam>
+public class BatchQueue<T> where T : class
+{
+  private readonly Channel<T> _channel;
+  private readonly int _capacity;
+  private readonly CancellationTokenSource _cts;
+
+  /// <summary>
+  /// バッチキューを初期化する
+  /// </summary>
+  /// <param name="capacity">キューの容量</param>
+  public BatchQueue(int capacity)
+  {
+    _capacity = capacity;
+    _cts = new CancellationTokenSource();
+
+    var options = new BoundedChannelOptions(capacity)
+    {
+      FullMode = BoundedChannelFullMode.Wait,
+      SingleReader = false,
+      SingleWriter = false
+    };
+
+    _channel = Channel.CreateBounded<T>(options);
+  }
+
+  /// <summary>
+  /// アイテムをキューに追加する
+  /// </summary>
+  /// <param name="item">追加するアイテム</param>
+  /// <returns>追加が成功したかどうかを示す非同期タスク</returns>
+  public async Task<bool> EnqueueAsync(T item)
+  {
+    try
+    {
+      await _channel.Writer.WriteAsync(item, _cts.Token);
+      return true;
+    }
+    catch (OperationCanceledException)
+    {
+      return false;
+    }
+    catch (ChannelClosedException)
+    {
+      return false;
+    }
+  }
+
+  /// <summary>
+  /// 複数のアイテムをキューに追加する
+  /// </summary>
+  /// <param name="items">追加するアイテムのコレクション</param>
+  /// <returns>追加が成功したかどうかを示す非同期タスク</returns>
+  public async Task<bool> EnqueueRangeAsync(IEnumerable<T> items)
+  {
+    try
+    {
+      foreach (var item in items)
+      {
+        await _channel.Writer.WriteAsync(item, _cts.Token);
+      }
+      return true;
+    }
+    catch (OperationCanceledException)
+    {
+      return false;
+    }
+    catch (ChannelClosedException)
+    {
+      return false;
+    }
+  }
+
+  /// <summary>
+  /// キューからアイテムを取得する
+  /// </summary>
+  /// <returns>キューから取得したアイテム</returns>
+  public async Task<T> DequeueAsync()
+  {
+    try
+    {
+      return await _channel.Reader.ReadAsync(_cts.Token);
+    }
+    catch (OperationCanceledException)
+    {
+      throw;
+    }
+    catch (ChannelClosedException)
+    {
+      throw;
+    }
+  }
+
+  /// <summary>
+  /// 指定された数のアイテムをキューから取得する
+  /// </summary>
+  /// <param name="count">取得するアイテムの数</param>
+  /// <param name="timeout">タイムアウト（ミリ秒）</param>
+  /// <returns>キューから取得したアイテムのリスト</returns>
+  public async Task<List<T>> DequeueMultipleAsync(int count, int timeout)
+  {
+    var result = new List<T>();
+    var timeoutToken = new CancellationTokenSource(timeout);
+    var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(_cts.Token, timeoutToken.Token);
+
+    try
+    {
+      for (int i = 0; i < count; i++)
+      {
+        if (_channel.Reader.TryRead(out var item))
+        {
+          result.Add(item);
+        }
+        else
+        {
+          try
+          {
+            if (await _channel.Reader.WaitToReadAsync(linkedCts.Token))
+            {
+              if (_channel.Reader.TryRead(out item))
+              {
+                result.Add(item);
+              }
+            }
+            else
+            {
+              break;
+            }
+          }
+          catch (OperationCanceledException)
+          {
+            break;
+          }
+        }
+      }
+    }
+    finally
+    {
+      timeoutToken.Dispose();
+      linkedCts.Dispose();
+    }
+
+    return result;
+  }
+
+  /// <summary>
+  /// キューが空かどうかを確認する
+  /// </summary>
+  /// <returns>キューが空の場合はtrue、それ以外の場合はfalse</returns>
+  public bool IsEmpty => _channel.Reader.Count == 0;
+
+  /// <summary>
+  /// キューに格納されているアイテムの数を取得する
+  /// </summary>
+  public int Count => _channel.Reader.Count;
+
+  /// <summary>
+  /// キューの容量を取得する
+  /// </summary>
+  public int Capacity => _capacity;
+
+  /// <summary>
+  /// キューを閉じる
+  /// </summary>
+  public void Close()
+  {
+    _channel.Writer.Complete();
+    _cts.Cancel();
+  }
+
+  /// <summary>
+  /// リソースを解放する
+  /// </summary>
+  public void Dispose()
+  {
+    _cts.Dispose();
+  }
+}
